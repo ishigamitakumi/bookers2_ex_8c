a3ff2ecde4adcd109b7e19175d158bce18d58118
EC2 Default User <ec2-user@ip-172-31-93-123.ec2.internal>
2022-02-09 11:25:02 +0000

    test

--- config/routes.rb
+++ config/routes.rb
@@ -15,6 +15,7 @@ Rails.application.routes.draw do
   end
   resources :groups, only: [:new, :index, :show, :create, :edit, :update] do
     resource :group_users, only: [:create, :destroy]
+    delete "all_destroy" => 'groups#all_destroy'
   end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   get '/search', to: 'searches#search'
